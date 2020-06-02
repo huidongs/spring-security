340444ba5f4dce82f8ef16d57e694ed7bbb72dce
Test <2804234243@qq.com>
2020-06-02 17:04:42 +0800

    Initial commit

---
+++ src/test/java/pers/huidong/springsecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.huidong.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
